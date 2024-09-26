574fcbe67528deb2360a88ecbc5bbbac01992efb
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-09-26 07:14:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
+import platform
 
 
 #import pickle5 as pickle
